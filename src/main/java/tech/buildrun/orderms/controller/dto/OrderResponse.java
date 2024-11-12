6f190a2290974f424aaa3fa765d551516c8f8424
Marcos Santos <marcossamuel1710@outlook.com>
2024-11-12 20:33:29 -0300

    feat: started new project

---
+++ src/main/java/tech/buildrun/orderms/controller/dto/OrderResponse.java
@@ -0,0 +1,13 @@
+package tech.buildrun.orderms.controller.dto;
+
+import tech.buildrun.orderms.entity.OrderEntity;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId,
+                            Long customerId,
+                            BigDecimal total) {
+    public static OrderResponse fromEntity(OrderEntity entity) {
+        return new OrderResponse(entity.getOrderId(), entity.getCustomerId(), entity.getTotal());
+    }
+}
---
+++ src/main/java/tech/buildrun/orderms/listener/dto/OrderCreatedEvent.java
@@ -0,0 +1,6 @@
+package tech.buildrun.orderms.listener.dto;
+
+import java.util.List;
+
+public record OrderCreatedEvent(Long codigoPedido, Long codigoCliente, List<OrderItemEvent> itens) {
+}
